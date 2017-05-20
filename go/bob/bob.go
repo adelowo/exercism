38531f2c750e5ca2739ae3f17b7027d38915187b
Lanre Adelowo <me@lanreadelowo.com>
2017-05-20 13:52:57 +0100

    Bob the lackadaisical teenager in Go

---
+++ go/bob/bob.go
@@ -0,0 +1,47 @@
+package bob
+
+import (
+	"strings"
+	"unicode"
+)
+
+const testVersion = 3
+
+func Hey(greeting string) string {
+	greeting = strings.TrimSpace(greeting)
+
+	if greeting == "" {
+		return "Fine. Be that way!"
+	}
+
+	if isQuestion(greeting) {
+		return "Whoa, chill out!"
+	}
+
+	if greeting[len(greeting)-1] == '?' {
+		return "Sure."
+	}
+
+	return "Whatever."
+
+}
+
+func isQuestion(s string) bool {
+	var upper, lower bool
+
+	for _, v := range s {
+		if unicode.IsUpper(v) {
+			upper = true
+			break
+		}
+	}
+
+	for _, v := range s {
+		if unicode.IsLower(v) {
+			lower = true
+			break
+		}
+	}
+
+	return upper && !lower
+}
