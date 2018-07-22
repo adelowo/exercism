3b4f0c6a30e7926a3e0c78511efddd2dc5a70f8f
Lanre Adelowo <yo@lanre.wtf>
2018-07-22 17:24:06 +0100

    difference of square in rust

---
+++ rust/difference-of-squares/src/lib.rs
@@ -0,0 +1,23 @@
+pub fn square_of_sum(n: usize) -> usize {
+    let mut total: usize = 0;
+
+    for i in 0..n + 1 {
+        total += i;
+    }
+
+    return total.pow(2);
+}
+
+pub fn sum_of_squares(n: usize) -> usize {
+    let mut total: usize = 0;
+
+    for i in 0..n + 1 {
+        total += i.pow(2)
+    }
+
+    return total;
+}
+
+pub fn difference(n: usize) -> usize {
+    return square_of_sum(n) - sum_of_squares(n);
+}
