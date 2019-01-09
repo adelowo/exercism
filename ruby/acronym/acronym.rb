0fe8240a8f81acacb304c036f69faa94c4e8ebe0
Lanre Adelowo <yo@lanre.wtf>
2019-01-09 20:07:10 +0100

    implement acronymn in ruby

---
+++ ruby/acronym/acronym.rb
@@ -0,0 +1,26 @@
+class Acronym
+  def self.abbreviate(word)
+    abbreviation = ''
+
+    word.split(' ').each do |w|
+      w.upcase!
+
+      add = false
+
+      splitted = w.split('-')
+
+      if splitted.length.eql? 2
+        abbreviation += "#{splitted[0][0]}#{splitted[1][0]}"
+        next
+      end
+
+      next unless w.split('').each do |char|
+        add = true if ('A'..'Z').cover? char.upcase
+      end
+
+      abbreviation += w[0] if add == true
+    end
+
+    abbreviation
+  end
+end
