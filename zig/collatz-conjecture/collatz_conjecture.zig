788b7d463f5b8cb52bbdf9301b526a63938acf9c
Lanre Adelowo <yo@lanre.wtf>
2024-06-08 01:37:54 +0100

    collatz conjecture in zig

---
+++ zig/collatz-conjecture/collatz_conjecture.zig
@@ -0,0 +1,36 @@
+const std = @import("std");
+
+pub const ComputationError = error{
+    IllegalArgument,
+};
+
+const defaultNumberOfSteps: usize = 0;
+
+pub fn steps(n: usize) anyerror!usize {
+    var numberOfSteps: usize = 0;
+
+    var number = n;
+
+    switch (number) {
+        0 => {
+            return ComputationError.IllegalArgument;
+        },
+        1 => {
+            return 0;
+        },
+        else => {
+            while (number != 1) {
+                numberOfSteps += 1;
+
+                if ((number % 2) == 0) {
+                    number = number / 2;
+                    continue;
+                }
+
+                number = (3 * number) + 1;
+            }
+        },
+    }
+
+    return numberOfSteps;
+}
